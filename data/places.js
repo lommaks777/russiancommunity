2a78a4d85dfc436e6ad42296a28c4f682438bd70
lommaks777 <106998043+lommaks777@users.noreply.github.com>
2025-08-31 00:54:56 -0300

    Update places.js

--- data/places.js
+++ data/places.js
@@ -36,42 +36,6 @@ window.PLACES = [
     "notes": ""
   },
   {
-    "id": 1756612105192,
-    "name": "Touchdown Bowling Metaponto",
-    "category": "Кофе",
-    "subtype": "",
-    "lat": -34.5863164,
-    "lng": -58.424268899999994,
-    "neighborhood": "Palermo",
-    "russian_score": 3,
-    "peak_hours": "",
-    "instagram": "",
-    "website": "https://www.parrilladonjulio.com/",
-    "phone": "+39 331 178 6681",
-    "photo_url": "https://places.googleapis.com/v1/places/ChIJW4I29DhpRxMREFWlu7Yh6LY/photos/ATKogpe1fgIkvRsxN8h762bJ61y4BLlRUZvE2LmL-Hct4TtbpRz8Sn4Hy8OfrNaPO3J1qE_aAtm8u58MYxeCkifaltzqTyhhCQiBxy1l91PyPainxobMcl31EX4DoMTucapfqaAKrpvj_V6JO8_ve3Mhv_G5sdsvdx8WejlAyA7h47v4sUx5-bf-ngFuAl525jSldO5MizoXZFDGu19B04zN5zd0rDscJJECskSp0jifXzq9OPu_wEQx9QebiuRG_961iFLxZd031iS20kZocZg35JSirRnftO1OMAVmE45tBbD4vg/media?maxWidthPx=800",
-    "address": "Uscita Pantanello, S.da Statale 106 Jonica, 75012 Metaponto MT, Италия",
-    "place_id": "ChIJoYl36oa1vJURWWhqD9z3UV8",
-    "notes": ""
-  },
-  {
-    "id": 1756605688495,
-    "name": "Tronador - Villa Ortuzar",
-    "category": "Кофе",
-    "subtype": "",
-    "lat": -34.5840955,
-    "lng": -58.4666862,
-    "neighborhood": "Villa Ortúzar",
-    "russian_score": 3,
-    "peak_hours": "",
-    "instagram": "",
-    "website": "",
-    "phone": "+54 800-555-1616",
-    "photo_url": "https://maps.googleapis.com/maps/api/place/js/PhotoService.GetPhoto?1sATKogpeJ3K4e0eoxXtxuSgQTRomBTfFg2kjaISC4YeHDpZiSFoOS80FP4m5R-3JBn7slB5Waw7UCCap9w8Ff2BGDp6gNbxIiJlhUQLO9lBnJXFy-WYQDoyfXU0ySsl3kM1MS41lwAaxmGxLKbGcDYOXYgblniQL4Usqjoky7ff_DXWqZE_HbR0JpdGwH85KKawfd9L-Zu5eagSOz8SuCtnoBQpeJglrv3TKFMf6tjZPU5mgwo8dfON0I-WBoVkjij3NkH1K1dSNLOPnCsz7Ca3j7uTZQBNVLluMkd1R1O3DNj-xr4ff9XkontDQUxjbXpmXOGdmgiFTy68NYsz4wR3_YOxq2TBjub7az4rmGju7eXk9KlPLhL9QROi6lhvHo0WDSswyXxJL9HXnipqsCmjhjm9Ec6_huY60tfmNYYG7zFh-hF6A&3u800&4u600&5m1&2e1&callback=none&r_url=http%3A%2F%2Frussian-community.com%2Fadd.html&key=AIzaSyB7-RgotVuWFR6qMvr_Alf8nQvkYd7I3WI&token=1345",
-    "address": "Av. Triunvirato 3100, C1427AAQ Cdad. Autónoma de Buenos Aires, Аргентина",
-    "place_id": "",
-    "notes": ""
-  },
-  {
     "id": 1756611784552,
     "name": "Консульский отдел посольства Российской федерации",
     "category": "Кофе",
@@ -88,22 +52,5 @@ window.PLACES = [
     "address": "Guido 1677, C1016AAC C1016AAC, Cdad. Autónoma de Buenos Aires, Аргентина",
     "place_id": "ChIJ9X2_jkPLvJURdWtfKIco1pI",
     "notes": ""
-  },
-  {
-    "id": 1,
-    "name": "Кофейня на углу",
-    "category": "Кофе",
-    "subtype": "Спешиалти",
-    "lat": -34.6037,
-    "lng": -58.3816,
-    "neighborhood": "Microcentro",
-    "russian_score": 4,
-    "peak_hours": "Сб–Вс 11:00–15:00",
-    "instagram": "",
-    "website": "",
-    "phone": "",
-    "photo_url": "",
-    "address": "",
-    "place_id": ""
   }
 ];
